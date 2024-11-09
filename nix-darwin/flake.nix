0fe04b3de231ae2dc39f938a7424cf8398ec5f50
Nick K <nick.kruchinin@gmail.com>
2024-11-09 15:55:53 +0300

    Initial commit

---
+++ nix-darwin/flake.nix
@@ -0,0 +1,74 @@
+{
+  description = "My Nix Darwin flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = [
+        pkgs.vim
+      ];
+
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+      # nix.package = pkgs.nix;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      homebrew = {
+        enable = true;
+        onActivation = {
+          autoUpdate = true;
+          upgrade = true;
+        };
+        # brews = [
+        #   "imagemagic",
+        #   "bat",
+        #   "siege",
+        #   "deno",
+        #   "youtube-dl",
+        #   "rustup",
+        #   "pulumi",
+        #   "htop",
+        #   "fnm",
+        #   "devcontainer",
+        #   "libsql/sqld/sqld",
+        #   "teamookla/speedtest/speedtest",
+        #   "oven-sh/bun/bun",
+        #   "git-extras"
+        # ];
+      };
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#macbook-pro
+    darwinConfigurations."macbook-pro" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."macbook-pro".pkgs;
+  };
+}
