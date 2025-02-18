2bdf256c3abfb50c37ced1a0ec4e42ea04f2d5c3
Nick K. <nick.kruchinin@gmail.com>
2025-02-18 23:36:13 +0300

    chore(nix): Remove unused package from devshell

--- flake.nix
+++ flake.nix
@@ -35,7 +35,6 @@
           default = pkgs.mkShell {
             packages = with pkgs; [
               git
-              node2nix
               nodejs
               nodePackages.pnpm
             ];
