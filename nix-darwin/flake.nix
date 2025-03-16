781a3c8e935be18de02701082ab98aa0a48af508
Nick K. <nick.kruchinin@gmail.com>
2025-03-17 00:19:06 +0300

    chore(nix-darwin): Update Nix version to nixVersions.latest

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -54,7 +54,7 @@
           home-manager.backupFileExtension = "backup";
 
           # Auto upgrade nix package and the daemon service.
-          nix.package = pkgs.nix;
+          nix.package = pkgs.nixVersions.latest;
 
           # Necessary for using flakes on this system.
           nix.settings.experimental-features = "nix-command flakes";
