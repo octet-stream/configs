3cd49cdf1f1c065e0a2caaef4c3faf3150719b7a
Nick K. <nick.kruchinin@gmail.com>
2025-03-20 16:59:43 +0300

    chore(nix): Use stable release from 24.11 branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Development environment for dotfiles";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
     utils.url = "github:numtide/flake-utils";
   };
 
