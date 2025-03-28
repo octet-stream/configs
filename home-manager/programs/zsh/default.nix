2c554857a365ec237ad5ff7856911c5222d8fbdc
Nick K. <nick.kruchinin@gmail.com>
2025-03-29 02:11:05 +0300

    chore(zsh): Update mac-rebuild alias to use nh

--- home-manager/programs/zsh/default.nix
+++ home-manager/programs/zsh/default.nix
@@ -32,7 +32,7 @@
         projects = "cd ~/projects";
         work = "cd ~/work";
         gpom = "git push origin main";
-        mac-rebuild = "darwin-rebuild switch --flake ~/projects/configs#macbook-pro";
+        mac-rebuild = "nh darwin switch";
         mac-up = "nix flake update --flake ~/projects/configs";
       };
     };
