2548d89f3299f5e6198c9a21c84fca0bbe34248c
Nick K. <nick.kruchinin@gmail.com>
2025-04-08 17:21:31 +0300

    chore(env): Use Node.js v22 in devshell

--- shell.nix
+++ shell.nix
@@ -7,9 +7,7 @@ mkShell {
     nixd
     nixfmt-rfc-style
     bash-language-server
-
-    # Corepack is broken prior v0.31.x, so use Node.js v23 for now, which includes it
-    nodejs_23
-    corepack_23
+    nodejs_22
+    corepack_22
   ];
 }
