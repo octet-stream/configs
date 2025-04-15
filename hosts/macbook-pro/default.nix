6af9191c06c7fef22df62750aaa919881aa88212
Nick K. <nick.kruchinin@gmail.com>
2025-04-15 05:30:20 +0300

    chore: Add comments regardless darwin modules

--- hosts/macbook-pro/default.nix
+++ hosts/macbook-pro/default.nix
@@ -1,9 +1,9 @@
 { inputs, ... }:
 {
   imports = with inputs; [
-    mac-app-util.darwinModules.default # Enables fix for darwin managed apps
-    nix-homebrew.darwinModules.nix-homebrew
-    home-manager.darwinModules.home-manager
+    mac-app-util.darwinModules.default # Applies a fix for the apps linken by nix-darwin
+    nix-homebrew.darwinModules.nix-homebrew # Adds homebrew installation manager
+    home-manager.darwinModules.home-manager # Adds Home Manager
 
     ./darwin
     ./home
