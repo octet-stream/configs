37292d3c3c4a97e900accc11ecd318d30fd21f0d
Nick K. <nick.kruchinin@gmail.com>
2025-04-30 11:38:28 +0300

    feat(ghostty): Add configuration

--- hosts/macbook-pro/home/programs/ghostty/settings.nix
+++ hosts/macbook-pro/home/programs/ghostty/settings.nix
@@ -1,6 +1,17 @@
+# Ghostty settings
+#
+# For more information on how to configure Ghostty,
+# check out documentation: https://ghostty.org/docs/config/reference#theme
+
+# ! Note: Remove ~/Library/Application\ Support/com.mitchellh.ghostty/config file if it exists, otherwise it will be loaded and break the whole configuration
+# ! See: https://github.com/ghostty-org/ghostty/discussions/5687
+
 { ... }:
 {
   programs.ghostty.settings = {
+    auto-update = "off";
+    theme = "Snazzy";
+
     # Window size
     window-width = 90;
     window-height = 20;
@@ -8,5 +19,6 @@
     # Cursor
     cursor-style = "block";
     cursor-style-blink = false;
+    shell-integration-features = "no-cursor"; # Disable shell integration for cursor, otherwise it will not pick up my settings
   };
 }
