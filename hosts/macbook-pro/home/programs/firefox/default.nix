9ef4842bb44011c6e174d1b17cb3f1f365f4c297
Nick K. <nick.kruchinin@gmail.com>
2025-05-02 11:51:48 +0300

    fix(firefox): Remove MOZ_LEGACY_PROFILES. add comments regarding the issue and firefox-bin package

--- hosts/macbook-pro/home/programs/firefox/default.nix
+++ hosts/macbook-pro/home/programs/firefox/default.nix
@@ -7,6 +7,19 @@
 
   programs.firefox = {
     enable = true;
-    package = pkgs.firefox-bin; # Override the package, because default package set doesn't support macOS
+
+    # Override the package, because default package doesn't support macOS.
+    #
+    # Note that [`firefox-bin`](https://github.com/bandithedoge/nixpkgs-firefox-darwin) is not universal,
+    # so the default version is set for Linux
+    package = if pkgs.stdenv.isDarwin then pkgs.firefox-bin else null;
   };
+
+  # ! To workaround these issues:
+  # ! https://github.com/bandithedoge/nixpkgs-firefox-darwin/issues/14
+  # ! https://github.com/nix-community/home-manager/issues/3323;
+  # !
+  # ! Disabled because it doesn't work :D
+  # ! But I'm gonna keep these comments for easy reference
+  # home.sessionVariables.MOZ_LEGACY_PROFILES = "1";
 }
