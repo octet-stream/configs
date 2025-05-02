53c8a5df59252324e94e4d5c5182cddaec6e086e
Nick K. <nick.kruchinin@gmail.com>
2025-05-02 16:31:18 +0300

    feat(starship): Add settings

--- hosts/macbook-pro/home/programs/starship/default.nix
+++ hosts/macbook-pro/home/programs/starship/default.nix
@@ -1,9 +1,9 @@
 { ... }:
 {
+  imports = [
+    ./settings.nix
+  ];
   programs.starship = {
     enable = true;
-    settings = {
-      add_newline = false;
-    };
   };
 }
---
+++ hosts/macbook-pro/home/programs/starship/settings.nix
@@ -0,0 +1,11 @@
+{ ... }:
+{
+  programs.starship.settings = {
+    add_newline = false; # Disable newline between prompts (because it adds one at the beginning of a new session)
+
+    character = {
+      success_symbol = "[➜](bold green)";
+      error_symbol = "[➜](bold red)";
+    };
+  };
+}
