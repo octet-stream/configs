4c597cd55fd049d1847b5f58c7190c6b523d6445
Nick K. <nick.kruchinin@gmail.com>
2025-05-29 02:51:50 +0300

    chore(deps): Bump flake.lock

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
 
     # Homebrew package
     homebrew = {
-      url = "github:Homebrew/brew/4.5.2"; # I'll have to bump the version by myself, because auto-updates are disabled
+      url = "github:Homebrew/brew/4.5.3"; # I'll have to bump the version by myself, because auto-updates are disabled
       flake = false;
     };
 
