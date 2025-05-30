03b44598c2f276d3681112f5667d86fb0c13604b
Nick K. <nick.kruchinin@gmail.com>
2025-05-30 21:29:34 +0300

    chore(firefox): Remove unused fix

--- hosts/macbook-pro/home/programs/firefox/default.nix
+++ hosts/macbook-pro/home/programs/firefox/default.nix
@@ -1,7 +1,6 @@
 { ... }:
 {
   imports = [
-    ./fixes
     ./extensions.nix
     ./settings.nix
   ];
@@ -9,12 +8,4 @@
   programs.firefox = {
     enable = true;
   };
-
-  # ! To workaround these issues:
-  # ! https://github.com/bandithedoge/nixpkgs-firefox-darwin/issues/14
-  # ! https://github.com/nix-community/home-manager/issues/3323;
-  # !
-  # ! Disabled because it doesn't work :D
-  # ! But I'm gonna keep these comments for easy reference
-  # home.sessionVariables.MOZ_LEGACY_PROFILES = "1";
 }
