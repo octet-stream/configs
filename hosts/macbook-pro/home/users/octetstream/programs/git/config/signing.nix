f11983a0c5e0f33c44f0617dcebe33862171617a
Nick K <nick.kruchinin@gmail.com>
2025-08-16 15:07:40 +0300

    fix(git): Add user email and signature path

--- hosts/macbook-pro/home/users/octetstream/programs/git/config/signing.nix
+++ hosts/macbook-pro/home/users/octetstream/programs/git/config/signing.nix
@@ -1,12 +1,17 @@
 { config, ... }:
+let
+  publicKeyPath = "${config.home.homeDirectory}/.ssh/id_gh_k.pub";
+  userEmail = "nick.kruchinin@gmail.com";
+in
 {
   programs.git = {
+    userName = "Nick K.";
+    userEmail = userEmail;
+
     signing = {
       format = "ssh";
       signByDefault = true;
-      key = "${config.users.users.octetstream.home}/.ssh/id_gh_k.pub";
+      key = publicKeyPath;
     };
-    userName = "Nick K.";
-    userEmail = null; # TODO: Add later
   };
 }
