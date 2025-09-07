7eabcbb54f03de2ff3cc564bb5560cef0f5c2b95
Nick K. <nick.kruchinin@gmail.com>
2025-09-07 17:46:10 +0300

    feat(firefox): Configure search engines shortcuts

--- hosts/macbook-pro/home/users/octetstream/programs/firefox/default.nix
+++ hosts/macbook-pro/home/users/octetstream/programs/firefox/default.nix
@@ -4,6 +4,7 @@
     ./extensions.nix
     ./pinned.nix
     ./settings.nix
+    ./search-engines.nix
   ];
 
   programs.firefox.enable = true;
---
+++ hosts/macbook-pro/home/users/octetstream/programs/firefox/search-engines.nix
@@ -0,0 +1,58 @@
+{ pkgs, ... }:
+{
+  programs.firefox.profiles.default.search.engines = {
+    nix-packages = {
+      name = "Nix Packages";
+      icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+      definedAliases = [ "@nixpkgs" ];
+      urls = [
+        {
+          template = "https://search.nixos.org/packages";
+          params = [
+            {
+              name = "type";
+              value = "packages";
+            }
+            {
+              name = "query";
+              value = "{searchTerms}";
+            }
+          ];
+        }
+      ];
+    };
+
+    home-manager-options = {
+      name = "Home Manager Options";
+      icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+      definedAliases = [ "@hmop" ];
+      urls = [
+        {
+          template = "https://home-manager-options.extranix.com";
+          params = [
+            {
+              name = "query";
+              value = "{searchTerms}";
+            }
+          ];
+        }
+      ];
+
+      youtube = {
+        name = "YouTube";
+        definedAliases = [ "@yt" ];
+        urls = [
+          {
+            template = "https://www.youtube.com/results";
+            arams = [
+              {
+                name = "search_query";
+                value = "{searchTerms}";
+              }
+            ];
+          }
+        ];
+      };
+    };
+  };
+}
