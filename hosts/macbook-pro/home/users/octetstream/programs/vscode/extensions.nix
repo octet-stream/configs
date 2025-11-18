07c89f04ab0ef5a7190ea4294bf5fa70cf02be13
Nick K. <nick.kruchinin@gmail.com>
2025-11-18 23:29:49 +0300

    chore(deps): Bump flake.lock and disable auto updates for vscode extension

--- hosts/macbook-pro/home/users/octetstream/programs/vscode/extensions.nix
+++ hosts/macbook-pro/home/users/octetstream/programs/vscode/extensions.nix
@@ -1,45 +1,48 @@
 # List of VSCode extensions managed by Nix sourced from nix-vscode-extensions and nix4vscode.
 
 { pkgs, ... }:
+let
+  # Extensions from nix-vscode-extensions overlay
+  defaultSource = with pkgs.vscode-marketplace; [
+    aaron-bond.better-comments
+    ahmadalli.vscode-nginx-conf
+    astro-build.astro-vscode
+    bierner.lit-html
+    biomejs.biome
+    dbaeumer.vscode-eslint
+    editorconfig.editorconfig
+    firefox-devtools.vscode-firefox-debug
+    github.vscode-github-actions
+    jnoortheen.nix-ide
+    ms-vscode.sublime-keybindings
+    orta.vscode-twoslash-queries
+    styled-components.vscode-styled-components
+    unifiedjs.vscode-mdx
+    vue.volar
+    yoavbls.pretty-ts-errors
+    ms-azuretools.vscode-containers
+  ];
+
+  # Extensions from nix4vscode
+  backupSource = pkgs.nix4vscode.forVscode [
+    "ms-vscode-remote.remote-containers"
+    "tamasfe.even-better-toml"
+    "mads-hartmann.bash-ide-vscode"
+    "redhat.vscode-yaml"
+    "waderyan.gitblame"
+    "catppuccin.catppuccin-vsc-icons"
+    "graphql.vscode-graphql"
+    "graphql.vscode-graphql-syntax"
+    "oxc.oxc-vscode"
+    "zenstack.zenstack"
+    "prisma.prisma"
+    "bradlc.vscode-tailwindcss"
+  ];
+in
 {
-  programs.vscode.profiles.default.extensions =
-    with pkgs.vscode-marketplace;
-    [ ]
+  programs.vscode.profiles.default = {
+    userSettings."extensions.autoUpdate" = false;
 
-    # Some extensions from this repo may break VSCode. If that happens - try and move them to the second list one by one
-    ++ [
-      aaron-bond.better-comments
-      ahmadalli.vscode-nginx-conf
-      astro-build.astro-vscode
-      bierner.lit-html
-      biomejs.biome
-      # bradlc.vscode-tailwindcss
-      dbaeumer.vscode-eslint
-      editorconfig.editorconfig
-      firefox-devtools.vscode-firefox-debug
-      github.vscode-github-actions
-      jnoortheen.nix-ide
-      ms-vscode.sublime-keybindings
-      orta.vscode-twoslash-queries
-      styled-components.vscode-styled-components
-      unifiedjs.vscode-mdx
-      vue.volar
-      yoavbls.pretty-ts-errors
-      ms-azuretools.vscode-containers
-    ]
-
-    ++ pkgs.nix4vscode.forVscode [
-      "ms-vscode-remote.remote-containers"
-      "tamasfe.even-better-toml"
-      "mads-hartmann.bash-ide-vscode"
-      "redhat.vscode-yaml"
-      "waderyan.gitblame"
-      "catppuccin.catppuccin-vsc-icons"
-      "graphql.vscode-graphql"
-      "graphql.vscode-graphql-syntax"
-      "oxc.oxc-vscode"
-      "zenstack.zenstack"
-      "prisma.prisma"
-      "bradlc.vscode-tailwindcss"
-    ];
+    extensions = defaultSource ++ backupSource;
+  };
 }
