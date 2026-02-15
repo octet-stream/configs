b6d3a4f93821b4257147f2820ca7f48cda07a062
Nick K. <nick.kruchinin@gmail.com>
2026-02-16 00:01:52 +0300

    chore(deps): Bump flake.lock and Remove Spark from mas dependencies

--- hosts/macbook-pro/darwin/homebrew/mas.nix
+++ hosts/macbook-pro/darwin/homebrew/mas.nix
@@ -7,7 +7,7 @@
     # Xcode = 497799835; # I don't remember why I needed XCode, so I commented it out
     Gifski = 1351639930;
     Lungo = 1263070803;
-    Spark = 1176895641;
+    # Spark = 1176895641;
     "HEIC Converter" = 1294126402;
     "Color Picker" = 1545870783;
   };
