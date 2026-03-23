3c59b568bb47bacb238d8113b4c0e5156edd4d8a
Nick K. <nick.kruchinin@gmail.com>
2026-03-23 21:13:05 +0300

    chore(deps): Bump flake.lock

--- hosts/macbook-pro/home/users/octetstream/programs/direnv/default.nix
+++ hosts/macbook-pro/home/users/octetstream/programs/direnv/default.nix
@@ -1,9 +1,11 @@
+{ pkgsUnstable, ... }:
 {
   config = {
     programs.direnv = {
       enable = true;
       enableZshIntegration = true;
       nix-direnv.enable = true;
+      package = pkgsUnstable.direnv;
     };
   };
 }
